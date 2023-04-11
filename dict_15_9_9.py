3a61f1f3a5640fe238c4527fa75e4c42114a60da
pjer <avdyunin@gmail.com>
2023-04-11 19:16:59 +0300

    add file dict_15_9_9

---
+++ dict_15_9_9.py
@@ -0,0 +1,11 @@
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+for a,b,c in zip(countries,capitals,population):
+    print(f"{b} is the capital of {a}, population equal {c} people.")
+
+
+
+
+
